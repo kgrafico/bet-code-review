ee68984d0d214e9b92d8537b1038af058eb450bc
Karolina Chamorro <carolinachasa@gmail.com>
2022-11-07 11:16:34 +0000

    Refactor crawler.ts
    Actions performed:
    
    - I have generated small functions to make the file more understandable
    - Import types file
    - Clean visited
    
    In conclusion, I have tried to clean the code to generate its more organized development since the function was getting complicated
    
    Thank you,
    Carolina

--- src/pages/api/util/crawler.ts
+++ src/pages/api/util/crawler.ts
@@ -1,79 +1,77 @@
 import { EventEmitter } from "node:events";
+import { StatusEvent } from "../../../types";
 import { crawlUrl } from "./http";
 
-export enum StatusEvent {
-  PROCESSING = "processing",
-  PROCESSED = "processed",
-  SEEN = "seen",
-}
-
-export default function runner(
+const Runner = (
   url: string,
   followUrlRestriction: any,
   maxConcurrentRequests: number
-) {
+) => {
   const status = new EventEmitter();
-  let pending: string[] = [];
+  EventEmitter.defaultMaxListeners = 20;
+  
   const seen: string[] = [];
-  const visited: string[] = [];
-
-  let cancelled = false;
-  let currentRequests = 0;
+  let pending: string[] = [];
   let pendingQueue: string[] = [];
+  let cancelled: boolean = false;
+  let currentRequests = 0;
 
-  async function processQueue() {
+  const processQueue = async () => {
+    const lastElementFromPendingQueue = pendingQueue.pop();
 
-    if (cancelled) {
-      return;
-    }
+    status.emit(StatusEvent.PROCESSING, lastElementFromPendingQueue);
 
-    const url = pendingQueue.pop();
-
-    if (!url) {
-      return;
-    }
-
-    status.emit(StatusEvent.PROCESSING, url);
-
-    pending = pending.filter(i => {return i !== url});
+    pending = pending.filter(i => i !== lastElementFromPendingQueue);
 
     currentRequests++;
 
-    const [links, body] = await crawlUrl(url);
+    await processingToProcessed(lastElementFromPendingQueue);
 
-    visited.push(url);
+    while (pendingQueue?.length > 0 && currentRequests < maxConcurrentRequests) {
+      processQueue();
+    }
+
+  }
+
+  const processingToProcessed = async (lastElementFromPendingQueue) => {
+    const [links, body] = await crawlUrl(lastElementFromPendingQueue);
+  
     currentRequests--;
+    status.emit(StatusEvent.PROCESSED, lastElementFromPendingQueue, body, links);
 
-    status.emit(StatusEvent.PROCESSED, url, body, links);
+    processedToSeen(links);
+  }
 
+  const processedToSeen = (links) => {
     links.forEach((link) => {
       if (!seen.includes(link)) {
+
         seen.push(link);
-        const shouldFollow =
-          !pending.includes(link) &&
-          !visited.includes(link) &&
-          new RegExp(followUrlRestriction).test(link);
+
+        const shouldFollow = !pending.includes(link) && new RegExp(followUrlRestriction).test(link);
+
         status.emit(StatusEvent.SEEN, link, shouldFollow);
+  
         if (shouldFollow) {
           pending.push(link);
           pendingQueue.push(link);
         }
       }
     });
-
-    while (pendingQueue.length > 0 && currentRequests < maxConcurrentRequests) {
-      processQueue();
-    }
   }
-
-  pending.push(url);
-  pendingQueue.push(url);
-  processQueue();
+  
+  if (!cancelled && url) {
+    pending.push(url);
+    pendingQueue.push(url);
+    processQueue();
+  }
 
   return {
     status,
-    cancel: function() {
+    cancel: () => {
       cancelled = true;
     }
   }
 }
+
+export default Runner;
