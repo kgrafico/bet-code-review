df86ab453b3db81bcd2fb85332ca6e55bf73ab28
Karolina Chamorro <carolinachasa@gmail.com>
2022-11-07 11:33:08 +0000

    Update index.tsx
    When refactoring a large component, one of the first things I would look for are business logic and functions that are not directly tied to the component state. 
    I have created small components to be able to have a smaller structure in which to organize our FrontEnd
    
    - Small component
    - Funcional Files
    -Types
    -Logical files
    
    Now the function and the business logic within it can be unit tested independently and reused by multiple components. If possible, keep functions pure they are deterministic and very easy to unit test.
    
    
    Thanks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import io, { Socket } from "socket.io-client";
+
+
+
+
+
+
+
+// ConnectedApp Component components/ConnectedApp.ts
 
 let socket: Socket;
 
-function HomePage() {
+const ConnectedApp = () => {
+  const [ seen, setSeen ] = useState<Array<string>>([]);
+  const [ isDisabled, setDisabled ] = useState<boolean>(false);
+  const [ processing, setProcessing ] = useState<string>('');
   
-  const [ seen, setSeen ] = useState([]);
-  const [ processing, setProcessing ] = useState('');
+  const [startCrawling, setStartCrawling] = useState<Object>({
+      url: "https://www.bbc.co.uk/news",
+      followUrlRestriction: "^https?://www.bbc.co.uk/news",
+      maxConcurrentRequests: 3
+    })
 
   const socketInitializer = async () => {
     await fetch("/api/socket");
@@ -21,39 +35,137 @@ function HomePage() {
     });
   };
 
-  useEffect(() => {
-    socketInitializer();
-  }, []);
-
-  function onStartCrawling() {
+  const cleanCrawiling = () => {
     setProcessing('');
     setSeen([]);
-    socket.emit(
-      "start",
-      "https://www.bbc.co.uk/news",
-      "^https?://www.bbc.co.uk/news",
-      3
-    );
   }
 
-  function onStopCrawling() {
+  const onStartCrawling = () => {
+    cleanCrawiling();
+    socket.emit("start", startCrawling);
+    toggleDisabled();
+  }
+
+  const toggleDisabled = () => {
+    setDisabled(!isDisabled);
+}
+
+  const onStopCrawling = () => {
     socket.emit("cancel");
+    cleanCrawiling();
+    toggleDisabled();
+    setProcessing('server stop')
   }
+  return ({
+    seen,
+    processing,
+    isDisabled,
+    socketInitializer,
+    setStartCrawling,
+    onStartCrawling,
+    onStopCrawling,
+  })
+}
 
+
+
+
+
+
+
+
+
+
+// ListComponent Component components/ListComponent.ts
+
+type ListComponentTypes = {
+  processing: string,
+  seen: Array<any>,
+}
+
+const ListComponent = (props: ListComponentTypes) => {
+  const {processing, seen} = props;
+  
   return (
     <div>
-      <h2>Crawler</h2>
-      <button onClick={onStartCrawling}>Start</button>
-      <button onClick={onStopCrawling}>Stop</button><br />
-      {processing && `Processing: ${processing}`}<br />
+      {seen.length ? `Processing: ${processing}` : processing}<br />
       {seen.length > 0 ?
         <ul>
-          {seen.map((link) => (
-            <li key={link}>{link}</li>
+          <p>Seen List:</p>
+          {seen.map((link, i) => (
+            <li key={i}>{link}</li>
           ))}
         </ul> : ''
       }
     </div>
+  );
+}
+
+
+
+
+
+
+
+
+// APP Component components/App.ts
+
+type PropsType = {
+  name: string,
+  processing: string,
+  seen: Array<string>,
+  isDisabled: boolean,
+  onStartCrawling: () => void,
+  onStopCrawling: () => void,
+
+}
+
+const App = (props: PropsType) => {
+  const {name, processing, seen, isDisabled, onStartCrawling, onStopCrawling} = props;
+  
+  // Improve TODO: use setStartCrawling to modify our startCrawling values
+  // Suggestion: add inputs to set: url, followUrlRestriction, and maxConcurrentRequests
+  
+  return (
+    <div>
+      <h2>{name}</h2>
+      <button onClick={onStartCrawling} disabled={isDisabled} >Start</button>
+      <button onClick={onStopCrawling} disabled={!isDisabled}>Stop</button><br />
+      <ListComponent
+        processing={processing}
+        seen={seen}
+      />
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+// HOMEPAGE Component index.ts
+
+const HomePage = () => {
+ const { processing, seen, isDisabled, onStartCrawling, socketInitializer, onStopCrawling } = ConnectedApp();
+
+  useEffect(() => {
+    socketInitializer();
+  }, []);
+
+  return (
+    <App 
+      name={'Crawler Project'}
+      processing={processing}
+      seen={seen}
+      isDisabled= {isDisabled}
+      onStartCrawling={onStartCrawling}
+      onStopCrawling={onStopCrawling}
+    />
   );
 }
 
